d6a2e1f68ca73d12f3382c5138b919702ee26444
Secundino Martinez <Dino.Martinez.514@gmail.com>
2021-01-29 00:16:46 -0800

    finished all challenges

--- challenges/challenge1.js
+++ challenges/challenge1.js
@@ -2,34 +2,45 @@
  *******************************************************************************
  * INSTRUCTIONS:
  * Follow the steps below and answer the discusssion questions that follow.
- * 
+ *
  * 1. Read over the code that follows. In what order will the outputs "Step 1",
  *    "Step 2", and "Step 3" be printed? How do you know?
- * 
- * 
+ *
+ *    It will will read 'Step 1, Step 3, Step 2' Because the second step will be
+ *    executed via setTimeout, which runs asynchronously. This means it allows
+ *    Step 3 to be printed while 'waiting' for the timeout to end and Step 2 to
+ *    be printed
+ *
  * 2. Run this code using `node challenge1.js`. In what order were the steps
  *    printed?
- * 
- * 
+ *
+ *    They were printed in the order I expected: Step 1, Step 3, Step 2
+ *
  * 3. Change the delay time in the `snooze` function from 2000 ms to 0. In what
  *    order will the steps be printed now? Why? Re-run the code again to verify
  *    your expectation. Were you correct?
- * 
+ *
+ *    They will run in the same order as before. This is because all synchronous
+ *    code is run first, before ANY async code. The setTimeout will be added to
+ *    a task list, which will be executed after one iteration of the event loop.
+ *    (NOTE: I just so happened to watch this video like, two days before this
+ *    lecture, lmao:
+ *    https://www.youtube.com/watch?v=vn3tm0quoqE&ab_channel=Fireship)
+ *     👍
  *******************************************************************************
  */
 
 /* This function takes a callback as a parameter. */
-function snooze(action) {
-    setTimeout(function() {
-      action();
-    }, 2000);
+function snooze (action) {
+  setTimeout(function () {
+    action()
+  }, 0)
 }
-console.log('Step 1');
+console.log('Step 1')
 
-snooze( function() {
-    console.log('Step 2');
-    console.log("Async Action completed via callback");
-} );
+snooze(function () {
+  console.log('Step 2')
+  console.log('Async Action completed via callback')
+})
 
-console.log('Step 3');
-
+console.log('Step 3')
--- challenges/challenge2.js
+++ challenges/challenge2.js
@@ -2,78 +2,105 @@
  *******************************************************************************
  * INSTRUCTIONS:
  * Follow the steps below and answer the discusssion questions that follow.
- * 
+ *
  * 1. Read over the code that follows. What will be printed to the console when
  *    it runs? Run the code using `node challenge2.js` and verify that your
  *    expectation was correct.
- * 
- * 
+ *
+ *    This will print 'Hello there, Ducky' and then 'MAKE SCHOOL IS AWESOME'
+ *    This is because we're using .then() which will wait until the Promise is
+ *    Resolved before doing the next step
+ *
  * 2. What happens if greet() fails? Make it fail by changing 'name' to a number
  *    instead of a string. What happens? Does uppercaser() still run?
- * 
- * 
+ *
+ *    If any Promise in the chain is rejected, the .catch() will halt all future
+ *    Promises. So uppercaser() will not run.
+ *
  * 3. What happens if greet() succeeds and uppercaser() fails? Modify your code
  *    to achieve this result by changing the values of 'name' and 'my_str' and
  *    run the code again.
- * 
- * 
+ *
+ *   The greet code will be executed, then the error log in the catch() will be
+ *   executed.
+ *
  * 4. Write a method that takes a string as input and returns the input string
  *    with a space added between each character. E.g. 'foo' -> 'f o o'
- * 
- *    Name this method spacer(str). It should run asynchronously, so use a 
+ *    Name this method spacer(str). It should run asynchronously, so use a
  *    setTimeout() and return a Promise.
- * 
  *    Last, call spacer() after you call greeter() and uppercaser().
- * 
  *    Make sure you only have one catch() block. If you have more than one,
- *    refactor your code so that you only have one. 
- * 
+ *    refactor your code so that you only have one.
+ *     👍 👍
+ *
  *******************************************************************************
  */
 
- /**
+/**
   * Asynchronously returns a greeting for a specified name.
   * @param name The name of the person to greet.
   */
-function greet(name) {
-    return new Promise(function(resolve, reject) {
-      setTimeout(function() {
-        if (typeof name === 'string') { 
-          resolve('Hello there, ' + name);
-        } else {
-          reject('Name must be a string!');
-        }
-      }, 1000);
-    });
+function greet (name) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      if (typeof name === 'string') {
+        resolve('Hello there, ' + name)
+      } else {
+        reject('Name must be a string!')
+      }
+    }, 1000)
+  })
 }
 
 /**
  * Returns the uppercased version of a string.
  * @param {*} str The string to uppercase.
  */
-function uppercaser(str) {
-    return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-        if (typeof str === 'string') {
-            resolve(str.toUpperCase());
-        } else {
-            reject('Argument to uppercaser must be string');
-        }
-        }, 1500);
-    });
+function uppercaser (str) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      if (typeof str === 'string') {
+        resolve(str.toUpperCase())
+      } else {
+        reject('Argument to uppercaser must be string')
+      }
+    }, 1500)
+  })
+}
+
+/**
+  * Asynchronously returns a spaced out version of your string.
+  * @param theThingYouWantSpacedOut The string you want spaced out.
+  */
+function spacer (theThingYouWantSpacedOut) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      if (typeof theThingYouWantSpacedOut === 'string') {
+        resolve(theThingYouWantSpacedOut.split('').join(' '))
+      } else {
+        reject('Name must be a string!')
+      }
+    }, 1000)
+  })
 }
 
 name = 'Ducky'
-my_str = 'Make School is Awesome!!!'
+my_str = 'Promises are fun!'
+notSpaced = 'abcdefghijklmnopqrstuvwxyz'
 
 greet(name)
-    .then((greetResult) => {
-        console.log(greetResult)
-        return uppercaser(my_str);
-    })
-    .then((uppercaserResult) => {
-        console.log(uppercaserResult)
-    }).catch((err) => {
-        console.log('Received an error!')
-        console.log(err);
-    });
+  .then((greetResult) => {
+    console.log(greetResult)
+    return uppercaser(my_str)
+  })
+  .then((uppercaserResult) => {
+    console.log(uppercaserResult)
+    return spacer(notSpaced)
+  })
+  .then((spacedResult) => {
+    console.log(spacedResult)
+  })
+  .catch((err) => {
+    console.log('Received an error!')
+    console.log(err)
+  })
